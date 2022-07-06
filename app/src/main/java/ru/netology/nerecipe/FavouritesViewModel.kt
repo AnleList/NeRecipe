5238900d526185ae53ee4f7804071db65731824e
Савосин Антон <anle@list.ru>
2022-07-06 20:25:20 +0300

    добавил нижнее меню с двумя пунктами, и фрагмент файворитс - пока пустой

---
+++ app/src/main/java/ru/netology/nerecipe/FavouritesViewModel.kt
@@ -0,0 +1,7 @@
+package ru.netology.nerecipe
+
+import androidx.lifecycle.ViewModel
+
+class FavouritesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/ru/netology/nerecipe/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nerecipe/ui/AppActivity.kt
@@ -1,6 +1,29 @@
 package ru.netology.nerecipe.ui
 
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import ru.netology.nerecipe.R
 
-class AppActivity : AppCompatActivity(R.layout.app_activity)
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_activity)
+        val navView: BottomNavigationView = findViewById(R.id.navigation)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.favouritesFragment,
+            R.id.feedFragment
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
